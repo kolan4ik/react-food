07dbd44a67ec712ea01dd74e0a2a9f563589fb7b
Nick Molodozhon <kolan4ik13@gmail.com>
2021-07-26 17:54:18 +0300

    fix ignore

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import { NotFound } from './pages/NotFound';
 function App() {
   return (
     <>
-      <Router>
+      <Router basename='/react-food'>
         <Header />
         <main className='container content'>
           <Switch>
